654566a496eeb180bb1f8432c98fc558d12bcb65
Jonathan Hernandez <jonathanmhernandez20@gmail.com>
2026-04-06 00:56:06 -0700

    inital commit

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -8,6 +8,30 @@ void parseCSV(const string&);
 
 int main() {
    parseCSV("students.csv");
-
+   
    return 0;
+}
+
+void parseCSV(const string&)
+{
+    ifstream file("students.csv");
+    string line;
+
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string token;
+
+        while (ss >> token)
+        {
+            size_t pos = token.find(':');
+
+            string key = token.substr(0, pos);
+            string value = token.substr(pos + 1);
+
+            cout << key << " -> " << value << endl;
+        }
+         cout << endl; // Add a newline after each line of the CSV
+    }
+   
 }
